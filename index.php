8efec9ff3f4df9ea9eacf521fcaf61e5a46b7739
MalicknND <ndiayemalick062@gmail.com>
2024-04-03 03:20:06 +0200

    save work

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./styles/style.css">
+    <!-- favicon -->
+    <link rel="shortcut icon" href="./images/favico.png" type="image/x-icon">
+    <title>Raccourcisseur d'url</title>
+</head>
+
+<body>
+    <section id="hello">
+        <div class="container">
+            <header>
+                <img src="./images//logo.png" alt="logo" id="logo">
+            </header>
+        </div>
+
+        <h1>Une url longue ? Raccourcissez là !</h1>
+        <h2>Largement meilleur et plus court que les autres.</h2>
+        <form action="index.php" method="POST">
+            <input type="url" name="url" id="url" placeholder="Collez un lien à raccourcir">
+            <input type="submit" value="Raccourcir">
+        </form>
+    </section>
+    <section id="brands">
+        <div class="container">
+            <h3>Ces marques nous font confiance</h3>
+            <img src="./images/1.png" alt="image" class="picture">
+            <img src="./images/2.png" alt="image" class="picture">
+            <img src="./images/3.png" alt="image" class="picture">
+            <img src="./images/4.png" alt="image" class="picture">
+        </div>
+    </section>
+    <footer>
+        <div class="container">
+            <img src="./images/logo2.png" alt="logo" id="logo">
+            <p>Copyright &copy; 2021 <NAME>
+            </p>
+            <a href="#">Contact</a> - <a href="#">A propos</a>
+        </div>
+    </footer>
+</body>
+
+</html>
